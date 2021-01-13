1684701b10e77ee9fdf059ce3d4e418625f8e296
Washington Guedes <avwl017@gmail.com>
2021-01-13 16:37:24 -0300

    adds lint

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const instance = firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-});
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.VUE_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.VUE_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+if (Object.values(config).some((x) => !x)) {
+  console.log(config);
+  throw new Error('Confira as configurações do Firebase no arquivo .env');
+}
+
+const instance = firebase.initializeApp(config);
 
 export default instance;
