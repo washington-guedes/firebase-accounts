5974811375aa9fb8671432e2d4e515879f7ee90c
Washington Guedes <avwl017@gmail.com>
2021-01-13 12:43:32 -0300

    adds firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const instance = firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+});
+
+export default instance;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import FirebaseInstance from './firebase';
+
 import App from './app.vue';
 import './app.scss';
 import { router } from './routing';
@@ -11,3 +13,5 @@ new Vue({
   router,
   render: (create) => create(App),
 }).$mount('#app');
+
+Vue.prototype.$auth = FirebaseInstance.auth();
